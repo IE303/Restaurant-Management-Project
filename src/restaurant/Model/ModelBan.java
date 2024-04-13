b399e47e607cc9c0e70dc2bdf26082bdf863ca31
Ho Tan Anh <anh152k3@gmail.com>
2024-04-13 14:46:02 +0700

    edit modelBan

--- src/restaurant/Model/ModelBan.java
+++ src/restaurant/Model/ModelBan.java
@@ -9,5 +9,62 @@ package restaurant.Model;
  * @author anh152003
  */
 public class ModelBan {
+    private int ID;
+    private String name; 
+    private String status; 
+    private int amountOfPeople;
+    private String position;
+
+    public ModelBan() {
+    }
+
+    public ModelBan(int ID, String name, String status, int amountOfPeople, String position) {
+        this.ID = ID;
+        this.name = name;
+        this.status = status;
+        this.amountOfPeople = amountOfPeople;
+        this.position = position;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getAmountOfPeople() {
+        return amountOfPeople;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAmountOfPeople(int amountOfPeople) {
+        this.amountOfPeople = amountOfPeople;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    
     
 }
